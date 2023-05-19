1db2e8890569159503b031913f804dcbfe588438
OH BYEONGYUN <code.by.o831@gmail.com>
2023-05-19 17:00:42 +0900

    ArrayList add 부분

--- src/jump_to_java/DataStructure.java
+++ src/jump_to_java/DataStructure.java
@@ -1,5 +1,50 @@
 package jump_to_java;
 
-public class DataStructure {
+import java.util.ArrayList;
 
+public class DataStructure {
+	
+	private void ExampleArray() {
+		int[] odd = {1,3,5,7,9};
+		String[] weeks = {"월","화","수","목","금","토","일"};
+		
+		String[] week = new String[7];
+		week[0] = "월";
+		week[1] = "화";
+		
+		//String[] week = new String[]; -> Corrupted Error
+		
+		// 반복문으로 출력할 떄
+		
+		for(int i = 0; i < week.length;i++) {
+			System.out.println(week[i]);
+		}
+		
+		
+	}
+	private void ExampleList() {
+		//리스트 자료형에서는 ArrayList, Vector, LinkedList 등의 자료형이 존재
+		
+		//ArrayList (import java.util.ArrayList)
+		
+		//add 메서드를 이용하여 데이터를 저장할 수 있다.
+		
+		/*
+		 * ArrayList<String> pitches = new ArrayList<>();
+		 * 
+		 * 위처럼 제네릭스를 사용하지 않으면 warning 발생
+		 * */
+		
+		ArrayList pitches = new ArrayList();
+		pitches.add("138");
+		pitches.add("129");
+		pitches.add("142");
+		
+		//get 메서드를 이용하여 특정 인덱스이 값 추출 가능
+	}
+	
+	
+	public static void main(String[] args) {
+		
+	}
 }
